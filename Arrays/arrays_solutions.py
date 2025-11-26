e0ff132273ccd6805b274f87d2dc66454c930b4e
Rohit Birdawade <rohitbirdawade2875@gmail.com>
2025-11-27 02:56:14 +0530

    Add base topic folders and LC001 Two Sum solution

---
+++ Arrays/arrays_solutions.py
@@ -0,0 +1,9 @@
+# 001 Two Sum
+class Solution_001:
+    def twoSum(self, nums, target):
+        hashmap = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in hashmap:
+                return [hashmap[diff], i]
+            hashmap[n] = i
